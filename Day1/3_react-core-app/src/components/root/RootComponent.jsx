b52f9a7b2295d6e58a7b303bdc797b75484fe42c
manishvbn <manishvbn@gmail.com>
2023-05-08 15:10:40 +0530

    Styles and CSS

--- Day1/3_react-core-app/src/components/root/RootComponent.jsx
+++ Day1/3_react-core-app/src/components/root/RootComponent.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import ComponentOne from '../2_multi-components/ComponentOne';
-import ComponentTwo from '../2_multi-components/ComponentTwo';
+
+// import ComponentOne from '../2_multi-components/ComponentOne';
+// import ComponentTwo from '../2_multi-components/ComponentTwo';
+
+// import ComponentOne from '../3_inline-styles/ComponentOne';
+// import ComponentTwo from '../3_inline-styles/ComponentTwo';
+
+// import ComponentOne from '../4_external-css/comp-one/ComponentOne';
+// import ComponentTwo from '../4_external-css/comp-two/ComponentTwo';
+
+import ComponentOne from '../5_css-modules/comp-one/ComponentOne';
+import ComponentTwo from '../5_css-modules/comp-two/ComponentTwo';
 
 const RootComponent = () => {
     return (
