390b50bd6191cbc0a90b4d5804d3c3a78dc50175
manishvbn <manishvbn@gmail.com>
2023-05-08 13:01:30 +0530

    ReactCoreApp

---
+++ Day1/3_react-core-app/src/components/1_hello/HelloComponent.jsx
@@ -0,0 +1,129 @@
+// import React from 'react';
+
+// class HelloComponent extends React.Component {
+//     render() {
+//         // Should return a single JSX Element
+//         return <h1>Hello World!</h1>;
+//     }
+// }
+
+// export default HelloComponent;
+
+// -------------------------------------------
+
+// import React, { Component } from 'react';
+
+// class HelloComponent extends Component {
+//     render() {
+//         // Should return a single JSX Element
+//         return <h1>Hello World!</h1>;
+//     }
+// }
+
+// export default HelloComponent;
+
+// -------------------------------------------
+
+// import React, { Component } from 'react';
+
+// class HelloComponent extends Component {
+//     render() {
+//         // Should return only a single JSX Element
+//         return (
+//             <div>
+//                 <h1>Hello World!</h1>
+//                 <h1>Hello World Again!</h1>
+//             </div>
+//         );
+//     }
+// }
+
+// export default HelloComponent;
+
+// ---------------------------------------- Fragment
+
+// import React, { Component } from 'react';
+
+// class HelloComponent extends Component {
+//     render() {
+//         // Should return only a single JSX Element
+//         return (
+//             <React.Fragment>
+//                 <h1>Hello World!</h1>
+//                 <h1>Hello World Again!</h1>
+//             </React.Fragment>
+//         );
+//     }
+// }
+
+// export default HelloComponent;
+
+// import React, { Component, Fragment } from 'react';
+
+// class HelloComponent extends Component {
+//     render() {
+//         // Should return only a single JSX Element
+//         return (
+//             <Fragment>
+//                 <h1>Hello World!</h1>
+//                 <h1>Hello World Again!</h1>
+//             </Fragment>
+//         );
+//     }
+// }
+
+// export default HelloComponent;
+
+// import React, { Component } from 'react';
+
+// class HelloComponent extends Component {
+//     render() {
+//         // Should return only a single JSX Element
+//         return (
+//             <>
+//                 <h1>Hello World!</h1>
+//                 <h1>Hello World Again!</h1>
+//             </>
+//         );
+//     }
+// }
+
+// export default HelloComponent;
+
+// ---------------------------------- Function Syntax
+
+// function HelloComponent() {
+//     return (
+//         <>
+//             <h1>Hello World!</h1>
+//             <h1>Function Declaration Syntax!</h1>
+//         </>
+//     );
+// }
+
+// const HelloComponent = function () {
+//     return (
+//         <>
+//             <h1>Hello World!</h1>
+//             <h1>Function Expression Syntax!</h1>
+//         </>
+//     );
+// }
+
+// const HelloComponent = () => {
+//     return (
+//         <>
+//             <h1>Hello World!</h1>
+//             <h1>Multiline Arrow Function Syntax!</h1>
+//         </>
+//     );
+// }
+
+const HelloComponent = () => (
+    <>
+        <h1>Hello World!</h1>
+        <h1>Singleline Arrow Function Syntax!</h1>
+    </>
+);
+
+export default HelloComponent;
