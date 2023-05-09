f9d3d0efc1294e33491b391b071fc3ad7c5fcaee
manishvbn <manishvbn@gmail.com>
2023-05-09 11:32:36 +0530

    Update CounterAssignment.jsx

--- Day2/1_react-core-app/src/components/3_counter-assignment/CounterAssignment.jsx
+++ Day2/1_react-core-app/src/components/3_counter-assignment/CounterAssignment.jsx
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this._clickCount = 0;
+        this.state = { count: 0, flag: false };
+        this._inc = this._inc.bind(this);
+        this._dec = this._dec.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    _manageClickCount(e) {
+        this._clickCount += 1;
+        if (this._clickCount > 9) {
+            this.setState({ flag: true });
+        }
+    }
+
+    _inc(e) {
+        this.setState({ count: this.state.count + this.props.interval }, () => {
+            this._manageClickCount(e);
+        });
+    }
+
+    _dec(e) {
+        this.setState({ count: this.state.count - this.props.interval }, () => {
+            this._manageClickCount(e);
+        });
+    }
+
     render() {
         return (
             <>
@@ -8,19 +37,46 @@ class Counter extends Component {
                     <h3 className="text-info">Counter Component</h3>
                 </div>
                 <div className="d-grid gap-2 mx-auto col-6">
-                    <input type="text" className="form-control form-control-lg" />
-                    <button className="btn btn-info">
+                    <input type="text" className="form-control form-control-lg" value={this.state.count} readOnly />
+                    <button className="btn btn-info" disabled={this.state.flag}
+                        style={this._style}
+                        onClick={this._inc}>
                         <span className='fs-4'>+</span>
                     </button>
-                    <button className="btn btn-info">
+                    <button className="btn btn-info" disabled={this.state.flag}
+                        style={this._style}
+                        onClick={this._dec}>
                         <span className='fs-4'>-</span>
                     </button>
-                    <button className="btn btn-secondary">
+                    <button className="btn btn-secondary" disabled={!this.state.flag}
+                        style={!this.state.flag ? { cursor: 'not-allowed', pointerEvents: 'all' } : {}}
+                        onClick={this.reset}>
                         <span className='fs-4'>Reset</span>
                     </button>
                 </div>
             </>
         );
+    }
+
+    reset(e) {
+        this._clickCount = 0;
+        this.setState({ count: 0, flag: false });
+    }
+
+    get _style() {
+        return this.state.flag ? { cursor: 'not-allowed', pointerEvents: 'all' } : {};
+    }
+
+    static get defaultProps() {
+        return {
+            interval: 1
+        };
+    }
+
+    static get propTypes() {
+        return {
+            interval: PropTypes.number
+        };
     }
 }
 
@@ -30,6 +86,8 @@ class CounterAssignment extends Component {
         return (
             <>
                 <Counter />
+                <hr />
+                <Counter interval={10} />
             </>
         );
     }
