50a196a3f28013e616f3424c559e3bbc1c2aa188
manishvbn <manishvbn@gmail.com>
2023-05-10 16:21:37 +0530

    Added HTTP Interceptor

--- Day3/2_react-routing-v5/react-routing-app/src/services/product-api-client.js
+++ Day3/2_react-routing-v5/react-routing-app/src/services/product-api-client.js
@@ -1,4 +1,37 @@
-import authenticatorClient from "./authenticator-api-client";
+// import authenticatorClient from "./authenticator-api-client";
+
+// const url = process.env.REACT_APP_PRODUCTS_API_URL;
+
+// const productApiClient = {
+//     getAllProducts: function () {
+//         return new Promise((resolve, reject) => {
+//             let fData = {
+//                 method: "GET",
+//                 headers: {
+//                     "accept": "application/json",
+//                     "x-access-token": authenticatorClient.getToken()
+//                 }
+//             };
+
+//             fetch(url, fData).then((response) => {
+//                 response.json().then((data) => {
+//                     if (response.status === 403)
+//                         reject(data.message);
+//                     else
+//                         resolve(data);
+//                 }).catch((err) => {
+//                     reject("Parsing Error");
+//                 })
+//             }).catch((err) => {
+//                 reject("Communication Error");
+//             });
+//         });
+//     }
+// };
+
+// export default productApiClient;
+
+// --------------------------------------------------------
 
 const url = process.env.REACT_APP_PRODUCTS_API_URL;
 
@@ -8,8 +41,7 @@ const productApiClient = {
             let fData = {
                 method: "GET",
                 headers: {
-                    "accept": "application/json",
-                    "x-access-token": authenticatorClient.getToken()
+                    "accept": "application/json"
                 }
             };
 
