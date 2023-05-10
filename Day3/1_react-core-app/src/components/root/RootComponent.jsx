185083a3733fb1434eff4c428a173680448ba68c
manishvbn <manishvbn@gmail.com>
2023-05-10 09:04:22 +0530

    Day 3 Init

---
+++ Day3/1_react-core-app/src/components/root/RootComponent.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import React from 'react';
+import ErrorHandler from '../common/ErrorHandler';
+
+const RootComponent = () => {
+    return (
+        <div className='container'>
+            <ErrorHandler>
+            </ErrorHandler>
+        </div>
+    );
+};
+
+export default RootComponent;
